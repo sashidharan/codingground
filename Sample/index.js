961a413c8080a68b54c82cdbf150c42a252645da
SK <sashi.it4@gmail.com>
2016-12-05 12:05:42 +0530

    Update index.js

--- Sample/index.js
+++ Sample/index.js
@@ -1,2 +1,9 @@
-/* Simple Hello World in Node.js */
-console.log("Hello World");
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.write(200);
+    res.write('<h1>Hello Sample</h1>');
+    res.end();
+});
+server.listen(3000, 'localhost', function (){
+    console.log('Server running at http://'+server.address().address+':'+server.address().port);
+});
